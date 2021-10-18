70a1e6b68beb81013a1b7d422b32dfd02f20800a
Wombatlord <jordan.stone0@gmail.com>
2021-10-18 06:06:01 +0100

    Created combined bar & pie figure. Added pretty colours to labels.

--- main.py
+++ main.py
@@ -30,7 +30,10 @@ def main(teams: List[str]) -> None:
     datasets = [guild.get_main_spec_dataset(_teams[team]) for team in teams]
     color_sequence = guild.sequence_role_colors(datasets[0], _teams[sys.argv[1]])
 
-    charts = [plots.BarChart(datasets[0], color_sequence), plots.PieChart(datasets[0], color_sequence), plots.Histogram(datasets[0])]
+    charts = [plots.BarChart(datasets[0], color_sequence),
+              plots.PieChart(datasets[0], color_sequence),
+              plots.Histogram(datasets[0]),
+              plots.CombinedPieBar(datasets[0], color_sequence)]
 
     for chart in charts:
         chart.render()
--- plots.py
+++ plots.py
@@ -32,6 +32,10 @@ class PieChart(Chart):
 
     def populate_chart(self) -> None:
         labels, values = self.normalise_dataset()
+        plt.rcdefaults()
+        plt.rcParams.update({'text.color': "xkcd:marine blue",
+                             'axes.labelcolor': "xkcd:marine blue"})
+
         fig, ax = plt.subplots(tight_layout=True)
         ax.pie(values, labels=labels, autopct='%1.1f%%', pctdistance=0.8, colors=self.colors)
         ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
@@ -54,6 +58,9 @@ class BarChart(Chart):
     def populate_chart(self) -> None:
         labels, values = self.normalise_dataset()
         plt.rcdefaults()
+        plt.rcParams.update({'text.color': "xkcd:marine blue",
+                             'axes.labelcolor': "xkcd:marine blue"})
+
         fig, ax = plt.subplots(tight_layout=True)
 
         y_pos = np.arange(len(labels))
@@ -64,7 +71,7 @@ class BarChart(Chart):
         ax.invert_yaxis()  # labels read top-to-bottom
         ax.set_xlabel('Mainspec BIS / Upgrade pieces awarded')
         ax.set_title('Loot Assignment Totals')
-        ax.set_facecolor('grey')
+        ax.set_facecolor('xkcd:cool gray')
         fig.patch.set_facecolor('grey')
 
     def render(self) -> None:
@@ -76,14 +83,51 @@ class BarChart(Chart):
         plt.savefig(f"charts/{team_name}-loot-bars")
 
 
+class CombinedPieBar(Chart):
+    def __init__(self, data: DataPoints, colors: List[str]):
+        self.data = data
+        self.colors = colors
+
+    def populate_chart(self) -> None:
+        labels, values = self.normalise_dataset()
+        plt.rcdefaults()
+        plt.rcParams.update({'text.color': "xkcd:marine blue",
+                             'axes.labelcolor': "xkcd:marine blue"})
+
+        px = 1 / plt.rcParams['figure.dpi']
+        fig, (bar, pie) = plt.subplots(1, 2, tight_layout=True, figsize=(1600*px, 800*px))
+        fig.suptitle("Bar & Pie")
+        fig.patch.set_facecolor('grey')
+
+        y_pos = np.arange(len(labels))
+        bar.barh(y_pos, values, align='center', color=self.colors)
+        bar.set_yticks(y_pos)
+        bar.set_yticklabels(labels)
+        bar.invert_yaxis()  # labels read top-to-bottom
+        bar.set_xlabel('Mainspec BIS / Upgrade pieces awarded')
+        bar.set_title('Loot Assignment Totals')
+        bar.set_facecolor('xkcd:cool gray')
+
+        pie.pie(values, labels=labels, autopct='%1.1f%%', pctdistance=0.8, colors=self.colors)
+        pie.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    def render(self) -> None:
+        self.populate_chart()
+        plt.show()
+
+    def save_chart(self, team_name: str) -> None:
+        self.populate_chart()
+        plt.savefig(f"charts/{team_name}-bar-and-pie")
+
+
 class Histogram(Chart):
     def __init__(self, data: DataPoints):
         self.data = data
 
     def populate_chart(self) -> None:
         values = self.normalise_dataset()[1]
-        fig, ax = plt.subplots()
         n_bins = max(values) + 1
+        fig, ax = plt.subplots()
         ax.set_title("Loot Histogram")
         ax.set_xlabel("Total Loot Awarded")
         ax.set_ylabel("Raiders")
