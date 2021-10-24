460a427756ee3cff08f29f36af1a8c8155eb5033
Tom Weldon <thomas.weldon@legl.com>
2021-10-25 00:47:14 +0100

    Prototyped a static default style dict config and apply_default_style method in chart

--- plots.py
+++ plots.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,16 +9,38 @@ DataPoints = List[Tuple[str, int]]
 DataSeries = Tuple[List[str], List[int]]
 
 
+class ChartColours(dict):
+    goldenrod = "xkcd:goldenrod"
+    ocean = "xkcd:ocean"
+    almost_black = "xkcd:almost black"
+    _config = {
+        "goldenrod": "xkcd:goldenrod",
+        "ocean": "xkcd:ocean",
+        "almost_black": "xkcd:almost black"
+    }
+
+    def __init__(self):
+        super().__init__(**self._config)
+
+    def __getattr__(self, item) -> str:
+        colour_spec = self.get(item)
+        if not colour_spec:
+            raise ValueError(f"No colour {item} is configured")
+        return colour_spec
+
+
 # noinspection PyTypeChecker
 class Chart:
+    _style: Optional[Dict[str, str]] = None
+    _team_id: str
     data: DataPoints
-    chart_colors: Dict[str, str] = {"goldenrod": "xkcd:goldenrod",
-                                    "ocean": "xkcd:ocean",
-                                    "almost_black": "xkcd:almost black"}
+    chart_colors: Dict[str, str] = ChartColours()
 
-    fonts: Dict[str, str] = {"monospace": "monospace",
-                             "roboto": "Roboto Mono Medium for Powerline",
-                             "inconsolata": "Inconsolata NF"}
+    fonts: Dict[str, str] = {
+        "monospace": "monospace",
+        "roboto": "Roboto Mono Medium for Powerline",
+        "inconsolata": "Inconsolata NF"
+    }
 
     def normalise_dataset(self) -> DataSeries:
         series: DataSeries = tuple([point[i] for point in self.data] for i in (0, 1))
@@ -30,17 +52,26 @@ class Chart:
     def render(self) -> None:
         raise NotImplementedError("Do not invoke the interface directly!")
 
-    def save_chart(self, team_name: str) -> None:
+    def save_chart(self) -> None:
         raise NotImplementedError("Do not invoke the interface directly!")
+
+    def apply_default_style(self):
+        if self._style:
+            plt.rcdefaults()
+            plt.rcParams.update(self._style)
 
     def apply_style(self, text_color, label_color, edge_color, title_color, font) -> None:
         plt.rcdefaults()
-        plt.rcParams.update({'text.color': text_color,
-                             'axes.labelcolor': label_color,
-                             'axes.edgecolor': edge_color,
-                             'axes.titlecolor': title_color,
-                             'font.family': font,
-                             'font.size': 12})
+        plt.rcParams.update(
+            {
+                'text.color': text_color,
+                'axes.labelcolor': label_color,
+                'axes.edgecolor': edge_color,
+                'axes.titlecolor': title_color,
+                'font.family': font,
+                'font.size': 12
+            }
+        )
 
     def apply_bar_style(self, figure, axes, title, xlabel, tick_colors, face_color) -> None:
         axes.set_title(title)
@@ -50,21 +81,33 @@ class Chart:
         axes.invert_yaxis()  # labels read top-to-bottom
         axes.set_facecolor(face_color)
         figure.patch.set_facecolor(face_color)
-        pass
+
+    @property
+    def team_id(self) -> str:
+        return self._team_id
+
+    @team_id.setter
+    def team_id(self, val: str) -> None:
+        self._team_id = val
 
 
 class PieChart(Chart):
+    _style = {
+        "text.color": ChartColours.ocean,
+        "axes.labelcolor": ChartColours.goldenrod,
+        "axes.edgecolor": ChartColours.goldenrod,
+        "axes.titlecolor": ChartColours.goldenrod,
+        "font.family": "inconsolata",
+        "font.size": 12,
+    }
+
     def __init__(self, data: DataPoints, role_colors: List[str]):
         self.data = data
         self.role_colors = role_colors
 
     def populate_chart(self) -> None:
         labels, values = self.normalise_dataset()
-        self.apply_style(self.chart_colors["ocean"],
-                         self.chart_colors["goldenrod"],
-                         self.chart_colors["goldenrod"],
-                         self.chart_colors["goldenrod"],
-                         self.fonts["inconsolata"])
+        self.apply_default_style()
 
         fig, ax = plt.subplots(tight_layout=True)
         fig.suptitle("Mainspec Loot Share", color=self.chart_colors["goldenrod"])
@@ -76,10 +119,9 @@ class PieChart(Chart):
         self.populate_chart()
         plt.show()
 
-    def save_chart(self, team_name: str):
+    def save_chart(self):
         self.populate_chart()
-        plt.savefig(f"{Config.charts_dir}/{team_name}-loot-pie.png")
-
+        plt.savefig(f"{Config.charts_dir}/{self.team_id}-loot-pie.png")
 
 class BarChart(Chart):
     def __init__(self, data: DataPoints, role_colors: List[str]):
@@ -88,11 +130,13 @@ class BarChart(Chart):
 
     def populate_chart(self) -> None:
         labels, values = self.normalise_dataset()
-        self.apply_style(self.chart_colors["goldenrod"],
-                         self.chart_colors["goldenrod"],
-                         self.chart_colors["ocean"],
-                         self.chart_colors["goldenrod"],
-                         self.fonts["inconsolata"])
+        self.apply_style(
+            self.chart_colors["goldenrod"],
+            self.chart_colors["goldenrod"],
+            self.chart_colors["ocean"],
+            self.chart_colors["goldenrod"],
+            self.fonts["inconsolata"]
+        )
 
         fig, ax = plt.subplots(tight_layout=True)
         y_pos = np.arange(len(labels))
@@ -101,20 +145,22 @@ class BarChart(Chart):
         ax.set_yticklabels(labels)
         ax.barh(y_pos, values, align='center', color=self.role_colors)
 
-        self.apply_bar_style(figure=fig,
-                             axes=ax,
-                             title="Fusion: Team Y Loot Assignment Totals",
-                             xlabel="Mainspec BIS / Upgrade pieces awarded",
-                             tick_colors=self.chart_colors["goldenrod"],
-                             face_color=self.chart_colors["almost_black"])
+        self.apply_bar_style(
+            figure=fig,
+            axes=ax,
+            title="Fusion: Team Y Loot Assignment Totals",
+            xlabel="Mainspec BIS / Upgrade pieces awarded",
+            tick_colors=self.chart_colors["goldenrod"],
+            face_color=self.chart_colors["almost_black"]
+        )
 
     def render(self) -> None:
         self.populate_chart()
         plt.show()
 
-    def save_chart(self, team_name: str) -> None:
+    def save_chart(self) -> None:
         self.populate_chart()
-        plt.savefig(f"{Config.charts_dir}/{team_name}-loot-bars")
+        plt.savefig(f"{Config.charts_dir}/{self.team_id}-loot-bars")
 
 
 class CombinedPieBar(Chart):
@@ -128,11 +174,13 @@ class CombinedPieBar(Chart):
         y_pos = np.arange(len(labels))
         px = 1 / plt.rcParams['figure.dpi']
 
-        self.apply_style(self.chart_colors["ocean"],
-                         self.chart_colors["goldenrod"],
-                         self.chart_colors["ocean"],
-                         self.chart_colors["goldenrod"],
-                         self.fonts["inconsolata"])
+        self.apply_style(
+            self.chart_colors["ocean"],
+            self.chart_colors["goldenrod"],
+            self.chart_colors["ocean"],
+            self.chart_colors["goldenrod"],
+            self.fonts["inconsolata"]
+        )
 
         fig, (bar, pie) = plt.subplots(1, 2, tight_layout=True, figsize=(1600 * px, 800 * px))
         fig.suptitle(f"Fusion: Team {self.main_title} Mainspec Loot Share", color=self.chart_colors["goldenrod"])
@@ -141,12 +189,14 @@ class CombinedPieBar(Chart):
         bar.set_yticks(y_pos)
         bar.set_yticklabels(labels)
 
-        self.apply_bar_style(figure=fig,
-                             axes=bar,
-                             title="Loot Assignment Totals",
-                             xlabel="Mainspec BIS / Upgrade pieces awarded",
-                             tick_colors=self.chart_colors["goldenrod"],
-                             face_color=self.chart_colors["almost_black"])
+        self.apply_bar_style(
+            figure=fig,
+            axes=bar,
+            title="Loot Assignment Totals",
+            xlabel="Mainspec BIS / Upgrade pieces awarded",
+            tick_colors=self.chart_colors["goldenrod"],
+            face_color=self.chart_colors["almost_black"]
+        )
 
         pie.pie(values, labels=labels, autopct='%1.1f%%', pctdistance=0.8, colors=self.role_colors)
         pie.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
@@ -155,9 +205,9 @@ class CombinedPieBar(Chart):
         self.populate_chart()
         plt.show()
 
-    def save_chart(self, team_name: str) -> None:
+    def save_chart(self) -> None:
         self.populate_chart()
-        plt.savefig(f"{Config.charts_dir}/{team_name}-bar-and-pie")
+        plt.savefig(f"{Config.charts_dir}/{self.team_id}-bar-and-pie")
 
 
 class Histogram(Chart):
@@ -168,11 +218,13 @@ class Histogram(Chart):
         values = self.normalise_dataset()[1]
         n_bins = max(values) + 1
 
-        self.apply_style(self.chart_colors["goldenrod"],
-                         self.chart_colors["goldenrod"],
-                         self.chart_colors["goldenrod"],
-                         self.chart_colors["goldenrod"],
-                         self.fonts["inconsolata"])
+        self.apply_style(
+            self.chart_colors["goldenrod"],
+            self.chart_colors["goldenrod"],
+            self.chart_colors["goldenrod"],
+            self.chart_colors["goldenrod"],
+            self.fonts["inconsolata"]
+        )
 
         fig, ax = plt.subplots()
 
@@ -189,6 +241,6 @@ class Histogram(Chart):
         self.populate_chart()
         plt.show()
 
-    def save_chart(self, team_name: str) -> None:
+    def save_chart(self) -> None:
         self.populate_chart()
-        plt.savefig(f"{Config.charts_dir}/{team_name}-histogram")
+        plt.savefig(f"{Config.charts_dir}/{self.team_id}-histogram")
